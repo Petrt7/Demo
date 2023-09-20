04d41fefb2dc9ff90f99bb3b2f0b35c808bd3d1d
PeterChen7 <peter120289@gmail.com>
2023-09-20 10:57:53 +0800

    day3

--- _assets/js/ch2ex2.js
+++
@@ -1,6 +0,0 @@
-let textarea = document.querySelector('#css-code-wrapper textarea');
-let style = document.querySelector('#example-css');
-textarea.innerHTML = style.textContent;
-textarea.addEventListener('change', function(){
-    style.textContent = textarea.value;
-});
